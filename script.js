feca75016c459d072aaeaf1beed1a2c3e698ba0f
T1712H70 Le Viet Dung <t1712h_lvdung@HN-APTECH.COM>
2018-05-12 11:22:02 +0700

    demoGit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	// $('#btnThaydoi').click(function(){
+
+	// $('p:lt(3)').html('<b>Noi dung moi</b>');
+
+	$('#hide').click(function(){
+		$("img").hide();
+	})
+
+	$('#show').click(function(){
+
+		$("img").show();
+	})
+
+
+	})
+
